3e7a7c83b9371d46dc04e7f8edebb3c745f1aeef
Remi Gillig <remigillig@gmail.com>
2014-11-11 21:26:42 +0000

    Fix bug with LoadLibrary being in a static constructor from inside a DLL. Removed FreeLibrary intentionally, let me know if you know a more elegant solution.

--- XInputInterface/GamePad.cpp
+++ XInputInterface/GamePad.cpp
@@ -1,4 +1,5 @@
 #include "GamePad.h"
+#include <cstdio>
 
 namespace
 {
@@ -9,25 +10,38 @@ namespace
 	{
 	public:
 		XInputLoader()
-			: mHandle(0), mGetState(0), mSetState(0)
+			: mLoaded(false), mHandle(0), mGetState(0), mSetState(0)
 		{
-			mHandle = LoadLibrary("xinput1_3.dll");
-
-			// Ordinal 100 is the same as XInputGetState but supports the Guide button.
-			mGetState = (XInputGetStatePointer)GetProcAddress(mHandle, (LPCSTR)100);
-
-			mSetState = (XInputSetStatePointer)GetProcAddress(mHandle, "XInputSetState");
 		}
 
 		~XInputLoader()
 		{
-			FreeLibrary(mHandle);
+		}
+
+		void ensureLoaded()
+		{
+			if (!mLoaded)
+			{
+				mHandle = LoadLibrary("xinput1_3.dll");
+				if (mHandle != NULL)
+				{
+					mGetState = (XInputGetStatePointer)GetProcAddress(mHandle, (LPCSTR)100); // Ordinal 100 is the same as XInputGetState but supports the Guide button.
+					mSetState = (XInputSetStatePointer)GetProcAddress(mHandle, "XInputSetState");
+					mLoaded = true;
+				}
+				else
+				{
+					printf_s("[XInput.NET] Failed to loaded xinput1_3.dll (error code 0x%08x, check that DirectX End-User Runtimes"
+						" is installed (http://www.microsoft.com/en-us/download/details.aspx?id=8109)\n", GetLastError());
+				}
+			}
 		}
 
 		XInputGetStatePointer mGetState;
 		XInputSetStatePointer mSetState;
 
 	private:
+		bool mLoaded;
 		HMODULE mHandle;
 	};
 
@@ -36,11 +50,13 @@ namespace
 
 DWORD XInputGamePadGetState(DWORD dwUserIndex, XINPUT_STATE* pState)
 {
+	gXInputLoader.ensureLoaded();
 	return gXInputLoader.mGetState(dwUserIndex, pState);
 }
 
 void XInputGamePadSetState(DWORD dwUserIndex, float leftMotor, float rightMotor)
 {
+	gXInputLoader.ensureLoaded();
 	XINPUT_VIBRATION vibration = { (int)(leftMotor * 65535), (int)(rightMotor * 65535) };
 	gXInputLoader.mSetState(dwUserIndex, &vibration);
 }
