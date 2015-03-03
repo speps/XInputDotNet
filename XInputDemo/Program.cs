fa5a2ae2683c7a442330aeaaa4f2d6150a14e562
Ben Hymers <b.hymers@gmail.com>
2015-03-03 09:57:01 +0000

    Fix incorrect display of buttons in XInputDemo
    (fix nicked from gridgem)

--- XInputDemo/Program.cs
+++ XInputDemo/Program.cs
@@ -14,7 +14,7 @@ namespace XInputDemo
                 Console.WriteLine("IsConnected {0} Packet #{1}", state.IsConnected, state.PacketNumber);
                 Console.WriteLine("\tTriggers {0} {1}", state.Triggers.Left, state.Triggers.Right);
                 Console.WriteLine("\tD-Pad {0} {1} {2} {3}", state.DPad.Up, state.DPad.Right, state.DPad.Down, state.DPad.Left);
-                Console.WriteLine("\tButtons Start {0} Back {1} LeftStick {2} RightStick {3} LeftShoulder {4} RightShoulder {5} Guide {6} A {7} B {9} X {9} Y {10}",
+                Console.WriteLine("\tButtons Start {0} Back {1} LeftStick {2} RightStick {3} LeftShoulder {4} RightShoulder {5} Guide {6} A {7} B {8} X {9} Y {10}",
                     state.Buttons.Start, state.Buttons.Back, state.Buttons.LeftStick, state.Buttons.RightStick, state.Buttons.LeftShoulder, state.Buttons.RightShoulder,
                     state.Buttons.Guide, state.Buttons.A, state.Buttons.B, state.Buttons.X, state.Buttons.Y);
                 Console.WriteLine("\tSticks Left {0} {1} Right {2} {3}", state.ThumbSticks.Left.X, state.ThumbSticks.Left.Y, state.ThumbSticks.Right.X, state.ThumbSticks.Right.Y);
